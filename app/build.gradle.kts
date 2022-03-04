79fc8fcb64ba74ff803562ca71021b3df5f660bf
roalcantara <rogerio.alcantara@gmail.com>
2022-03-04 04:33:35 -0300

    chore(deps): Add kotlinx.coroutines
    Library support for Kotlin coroutines.
    
    More: https://kotlinlang.org/docs/coroutines-basics.html#your-first-coroutine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
+
+    // Library support for Kotlin coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 }
 
 testing {
